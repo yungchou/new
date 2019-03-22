09c5543157b3154159f2f4adc01eae27fc77755c
yc2121 <yc2121@live.com>
2019-03-22 09:04:16 -0500

    minis

---
+++ mnist.R
@@ -0,0 +1,36 @@
+library(keras)
+mnist <- dataset_mnist()
+
+str( train_images <- mnist$train$x )
+plot(as.raster(train_images[1,,],max=255))
+str( train_labels <- mnist$train$y )
+
+str( test_images  <- mnist$test$x )
+str( test_labels  <- mnist$test$y )
+
+network <- keras_model_sequential() %>%
+  layer_dense(units = 512, activation = 'relu',
+              input_shape = c(28*28)) %>%
+  layer_dense(units = 10, activation = 'softmax')
+
+network %>% compile(
+  optimizer = 'rmsprop',
+  loss = 'categorical_crossentropy',
+  metrics = c('accuracy')
+)
+
+train_images <- array_reshape(train_images, c(60000,28*28))
+train_images <- train_images/255
+
+test_images  <- array_reshape(test_images, c(10000,28*28))
+test_images  <- test_images/255
+
+train_labels <- to_categorical(train_labels)
+test_labels  <- to_categorical(test_labels)
+
+network %>% fit(train_images,train_labels, epochs=5, batch_size=128)
+
+metrics <- network %>% evaluate(test_images, test_labels, verbose=FALSE)
+metrics
+
+network %>% predict_classes(test_images[1:10,])
