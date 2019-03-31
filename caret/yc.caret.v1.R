eb9fc65e7584663859ad39b6527ea24e56acabf3
yc2121 <yc2121@live.com>
2019-03-31 14:23:41 -0500

    caret

--- caret/yc.caret.v1.R
+++ caret/yc.caret.v1.R
@@ -13,3 +13,36 @@ source("./helper/yc.helper.R");
 missingness(df)
 mydf <- splitting(df, training=0.6, testing=0.2, holding=0.2)
 
+# Recursive Feature Elimination Useing Caret
+if (!require('caret')) install.packages('caret', dependencies=c("Depends","Suggests") ); library(caret  )
+
+set.seed(1.1)
+
+#---------------------------
+# REMOVE REDUNDANT FEATURES
+#---------------------------
+
+# ensure the results are repeatable
+
+library(mlbench)
+data(PimaIndiansDiabetes)
+
+# calculate correlation matrix
+correlationMatrix <- cor(PimaIndiansDiabetes[,1:8])
+# summarize the correlation matrix
+print(correlationMatrix)
+# find attributes that are highly corrected (ideally >0.75)
+highlyCorrelated <- findCorrelation(correlationMatrix, cutoff=0.5)
+# print indexes of highly correlated attributes
+print(highlyCorrelated)
+
+# prepare training scheme
+control <- trainControl(method="repeatedcv", number=10, repeats=3)
+# train the model
+model <- train(diabetes~., data=PimaIndiansDiabetes, method="lvq", preProcess="scale", trControl=control)
+# estimate variable importance
+importance <- varImp(model, scale=FALSE)
+# summarize importance
+print(importance)
+# plot importance
+ggplot(importance)
