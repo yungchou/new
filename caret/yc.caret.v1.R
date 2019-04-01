5baeba691a09fdfde48eeda9135737f7d6a72fdc
yc2121 <yc2121@live.com>
2019-03-31 23:56:45 -0500

    caret

--- caret/yc.caret.v1.R
+++ caret/yc.caret.v1.R
@@ -46,3 +46,16 @@ importance <- varImp(model, scale=FALSE)
 print(importance)
 # plot importance
 ggplot(importance)
+
+
+
+# define the control using a random forest selection function
+control <- rfeControl(functions=rfFuncs, method="cv", number=10)
+# run the RFE algorithm
+results <- rfe(PimaIndiansDiabetes[,1:8], PimaIndiansDiabetes[,9], sizes=c(1:8), rfeControl=control)
+# summarize the results
+print(results)
+# list the chosen features
+predictors(results)
+# plot the results
+plot(results, type=c("g", "o"))
